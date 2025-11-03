2d455695d05e9bc4eb6c8d632670347678c85a6b
Lucero <1641249@senati.pe>
2025-11-03 11:31:47 -0500

    feat: Enviando vistas de Login y Success

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home  from "./pages/Home";
+import Home from "./pages/home";
+import Login from "./pages/Login";
+import LoginSuccess from "./pages/LoginSuccess";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/LoginSuccess" element={<LoginSuccess />} />
       </Routes>
     </BrowserRouter>
   );
--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 function Home(){
+    const [token, setToken] = useState(null);
+    useEffect(() =>{
+        const storedToken = localStorage.getItem('autnToken');
+        if(storedToken){
+            setToken(storedToken);
+        }
+    }), [];
+
+    const handleLogout = () =>{
+        localStorage.removeItem('authToken');
+        setToken(null);
+    }
+
     return(
         <div className="min-h-screen bg-gray-100 p-8">
             <h1 className="text-2xl font-bold mb-4">Bienvenida a mi pagina 💙</h1>
+            {(token && (
+                <button onClick={handleLogout}
+                className="px-4 py-2 text-white bg-orange-500 rounded hover:bg-orange-600">Cerrar sesión</button>
+            )
+            )}
+            {token ? (
+                <div>
+                    <p className="text-green-600 text-xl">Haz iniciado exitosamente</p>
+                </div>
+            ):(
+                <div>
+                    <p className="text-red-600 text-xl">Parece que no haz iniciado sesión</p>
+                    <Link to='/Login' className="mt-4 inline-bock text-blue-500">Ir a la página de Login</Link>
+                </div>
+            )}
             <p className="text-blue-600 text-xl">Esta es nuestra aplicacion de tienda de celulares 📱</p>
         </div>
     );
